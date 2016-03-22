d30fbc19dc670c9d8f6fd1c11f8647ec1243f5fa
mctribble <techgeek1000@gmail.com>
2016-03-22 15:02:48 -0700

    Added a DiscardRandomCard effect and a card to use it.

--- Assets/Scripts/Effects/EffectsDiscard.cs
+++ Assets/Scripts/Effects/EffectsDiscard.cs
@@ -21,22 +21,3 @@ class EffectReturnsToTopOfDeck : IEffectDiscard
         return true; //tell the card it no longer needs to be discarded
     }
 }
-
-//adds x charges to the card
-class EffectAddCharges : IEffectDiscard
-{
-    //generic interface
-    public TargetingType targetingType { get { return TargetingType.none; } }   //this effect doesnt need a target
-    public EffectType effectType { get { return EffectType.discard; } }         //this is a discard effect
-    public float strength { get; set; }                                         //how strong this effect is
-
-    //this effect
-    public string Name { get { return "Card gains " + strength + "charges."; } }        //returns name and strength
-    public bool trigger(ref Card c)
-    {
-        Debug.Log(c.charges);
-        c.charges += Mathf.RoundToInt(strength);
-        Debug.Log(c.charges);
-        return false; //the card should be discarded as normal
-    }
-}
