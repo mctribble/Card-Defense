fd57ce8410bf64a059a8256671c3c10f85bceb32
mctribble <techgeek1000@gmail.com>
2016-07-29 16:10:20 -0700

    added ScaleAttackWithHealth effect.  unused.

--- Assets/Scripts/Effects/EffectsEnemyReachedGoal.cs
+++ Assets/Scripts/Effects/EffectsEnemyReachedGoal.cs
@@ -5,6 +5,31 @@ using System;
 
 //effects in this file take effect when the enemy reaches its goal
 
+//enemy attack increases as it takes damage (range: 1x to 2x, rounds down)
+public class EffectScaleAttackWithHealth: IEffectEnemyReachedGoal
+{
+    [Hide] public TargetingType targetingType { get { return TargetingType.none; } }    //this effect doesnt need a target
+    [Hide] public EffectType effectType { get { return EffectType.enemyReachedGoal; } } //effect type
+    [Show, Display(2)] public float strength { get; set; }                              //effect strength (unused in this effect)
+    [Hide] public string argument { get; set; }                                         //effect argument (unused in this effect)
+
+    [Hide] public string Name
+    {
+        get
+        {
+            return "attack increases as the enemy gets damaged.";
+        }
+    }
+
+    [Show, Display(1)] public string XMLName { get { return "scaleAttackWithHealth"; } }
+
+    public void trigger(EnemyScript enemy)
+    {
+        enemy.damage = enemy.damage + Mathf.FloorToInt(enemy.damage * (((float)enemy.curHealth) / ((float)enemy.maxHealth)));
+    }
+}
+
+//enemy attack decreases as it takes damage (range: 1x to 0x, rounds up)
 public class EffectinvScaleAttackWithHealth: IEffectEnemyReachedGoal
 {
     [Hide] public TargetingType targetingType { get { return TargetingType.none; } }    //this effect doesnt need a target
