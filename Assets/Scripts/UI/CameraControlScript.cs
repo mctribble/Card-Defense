71ea9bfeea8edf2e38d2b72e0595958c0ffe2d08
mctribble <techgeek1000@gmail.com>
2016-10-28 18:01:42 -0700

    camera control script automatically saves a screenshot of the level when it loads.

--- Assets/Scripts/UI/CameraControlScript.cs
+++ Assets/Scripts/UI/CameraControlScript.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Vexe.Runtime.Types;
+using System.IO;
 
 /// <summary>
 /// handles user input to control the camera
 /// </summary>
 public class CameraControlScript : BaseBehaviour
 {
+    public Canvas UICanvas; //reference to the UI canvas so we can hide it for screenshots
+
     public float cameraSpeed;  //speed of the camera
     public Rect  cameraBounds; //region the camera is allowed to move in
 
@@ -14,13 +17,16 @@ public class CameraControlScript : BaseBehaviour
     public float minSize;   //min size of the camera
     public float maxSize;   //max size of the camera
 
+    public float cameraPadding;  //how much padding to add when adjusting the camera to see the entire level
+    public string thumbnailPath; //where to save level thumbnails
+
     private Camera cameraRef;
 
     //init
     private void Start()
     {
         cameraRef = this.GetComponent<Camera>();
-        LevelManagerScript.instance.LevelLoadedEvent += showEntireLevel; //register the level loaded event so that we can move the camera as needed to see everything when it loads
+        LevelManagerScript.instance.LevelLoadedEvent += levelLoadedHandler; //register the level loaded event so that we can move the camera as needed to see everything when it loads
     }
 
     //allows player to move the camera
@@ -43,9 +49,19 @@ public class CameraControlScript : BaseBehaviour
         cameraRef.orthographicSize  = Mathf.Clamp(cameraRef.orthographicSize, minSize, maxSize);
     }
 	
+    /// <summary>
+    /// called when a level is loaded.  Recenters the camera to show the whole level and also updates the level thumbnail in case something has changed since the last load
+    /// TODO: consider removing the thumbnail update when the game releases
+    /// </summary>
+    private void levelLoadedHandler()
+    {
+        showEntireLevel();
+        saveLevelThumbnail();
+    }
+
     ///<summary>
     ///moves/zooms the camera to show the entire level
-    ///this is called automatically when a level is loaded.
+    ///if there is no level loaded, logs a warning instead
     ///</summary>
     [Show] public void showEntireLevel()
     {
@@ -62,8 +78,35 @@ public class CameraControlScript : BaseBehaviour
 
             float WidthAspect = levelBounds.width / cameraRef.aspect;     //this is how high the screen would have to be to contain the entire width of the level
             float minHeight = Mathf.Max(levelBounds.height, WidthAspect); //so the screen has to be at least this big
-            float desiredScreenHeight = minHeight + 3.0f;                 //but we'll also give it a little padding so it doesnt look cramped
+            float desiredScreenHeight = minHeight + cameraPadding;                 //but we'll also give it a little padding so it doesnt look cramped
             cameraRef.orthographicSize = desiredScreenHeight / 2.0f;      //and the orthographic size we set is half of that
+        }
+        else
+        {
+            Debug.LogWarning("could not showEntireLevel() because there is no level to show or the level has no paths.");
+        }
+    }
+
+    /// <summary>
+    /// takes a screenshot of the level and saves it as a .png with the same name as the level to StreamingAssets\Level Thumbnails
+    /// if there is no level loaded, logs a warning instead
+    /// </summary>
+    [Show] public void saveLevelThumbnail()
+    {
+        if ( LevelManagerScript.instance.levelLoaded )
+        {
+            string screenshotName = Path.Combine(Application.dataPath, thumbnailPath); //find the folder we're saving to
+            screenshotName = Path.Combine(screenshotName, Path.GetFileNameWithoutExtension(LevelManagerScript.instance.data.fileName)); //add the file name of the level
+            screenshotName += ".png"; //add the extension
+
+            //take the screenshot
+            UICanvas.enabled = false;
+            Application.CaptureScreenshot(screenshotName);
+            UICanvas.enabled = true;
+        }
+        else
+        {
+            Debug.LogWarning("could not saveLevelThumbnail() because there is no level loaded.");
         }
     }
 }
