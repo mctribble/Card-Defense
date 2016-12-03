84f385b3c04769c0a42cb256166940fda4931806
Christopher Kaleb Hake <techgeek1000+github@gmail.com>
2016-12-02 18:58:27 -0800

    minor tweak to wave generation

--- Assets/Scripts/Levels/LevelManagerScript.cs
+++ Assets/Scripts/Levels/LevelManagerScript.cs
@@ -105,9 +105,9 @@ public class LevelData
 
     //budget: absolute + (wave * linear) + (wave * squared)^2 + (exponential^wave)
     [DefaultValue(100.0f)] public float waveGrowthAbsolute     = 100.0f;
-    [DefaultValue(7.0f)]   public float waveGrowthLinear       = 7.0f;
-    [DefaultValue(2.8f)]   public float waveGrowthSquared      = 2.8f;
-    [DefaultValue(1.3f)]   public float waveGrowthExponential  = 1.3f;
+    [DefaultValue(8.0f)]   public float waveGrowthLinear       = 8.0f;
+    [DefaultValue(2.7f)]   public float waveGrowthSquared      = 2.7f;
+    [DefaultValue(1.2f)]   public float waveGrowthExponential  = 1.2f;
 
     //time: min(wave*linear, maxwavetime)
     [DefaultValue(1.1f)]  public float waveTimeLinear = 1.1f;
